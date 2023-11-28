ff03ba49e5d44c72323e53c63a7f9e60d3cad2aa
asamiiii <mahrmlolo@gmail.com>
2023-11-28 09:33:07 +0200

    add images section

--- lib/screens/items_screen/item_details.dart
+++ lib/screens/items_screen/item_details.dart
@@ -3,6 +3,8 @@ import 'package:image_network/image_network.dart';
 
 class ItemDetails extends StatelessWidget {
   const ItemDetails({super.key});
+  
+  get itemCount => null;
 
   @override
   Widget build(BuildContext context) {
@@ -12,15 +14,19 @@ class ItemDetails extends StatelessWidget {
       child: Column(
         children: [
           Container(
-            color: Colors.greenAccent,
-            height: 350,
-            width: 350,
+            
+            decoration: BoxDecoration(
+              color: Colors.greenAccent,
+              borderRadius: BorderRadius.circular(25)
+            ),
+            height: 420,
+            width: 320,
             padding: const EdgeInsets.all(8),
             child: Row(
               children: [
                 Column(
                   children: [
-                    Text('Main Image'),
+                    Text('Main Image',style: TextStyle(fontWeight: FontWeight.bold),),
                     SizedBox(height: 10),
                     ClipOval(
                         child: ImageNetwork(
@@ -32,32 +38,29 @@ class ItemDetails extends StatelessWidget {
                     SizedBox(
                       height: 10,
                     ),
-                    Text('Details Images '),
-                    Row(
-                      children: [
-                        ClipOval(
-                            child: ImageNetwork(
-                                image:
-                                    'https://selectitaly.com/blog/wp-content/uploads/ferrero-rocher-3992585_1920.jpg',
-                                height: 50,
-                                width: 50)),
-                        ClipOval(
-                            child: ImageNetwork(
-                                image:
-                                    'https://selectitaly.com/blog/wp-content/uploads/ferrero-rocher-3992585_1920.jpg',
-                                height: 50,
-                                width: 50)),
-                        ClipOval(
-                            child: ImageNetwork(
-                                image:
-                                    'https://selectitaly.com/blog/wp-content/uploads/ferrero-rocher-3992585_1920.jpg',
-                                height: 50,
-                                width: 50))
-                      ],
-                    ),
-                    SizedBox(
+                    const Text('Details Images ',style: TextStyle(fontWeight: FontWeight.bold),),
+                    const SizedBox(
                       height: 10,
                     ),
+                    SizedBox(
+                      height: 100,
+                      width: 300,
+                      child: ListView.separated(
+                        shrinkWrap: true,
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (context, index) => ClipOval(
+                              child: ImageNetwork(
+                                  image:
+                                      'https://selectitaly.com/blog/wp-content/uploads/ferrero-rocher-3992585_1920.jpg',
+                                  height: 100,
+                                  width: 100)), 
+                                  separatorBuilder: (context, index) => SizedBox(width: 5,), itemCount: 3),
+                    ),
+                                
+                    SizedBox(
+                      height: 5,
+                    ),
+                    TextButton(onPressed: () {}, child: Text('Change')),
                   ],
                 ),
               ],
